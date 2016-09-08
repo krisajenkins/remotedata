8cf5409ce8e72aea68929fdc7118afd9dcdd3b3d
Kris Jenkins <krisajenkins@gmail.com>
2016-09-08 12:19:00 +0100

    Simplifying the emacs local config.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil (eval local-set-key
-            (kbd "M-e")
-            '(lambda ()
-               (interactive)
-               (require 'magit)
-               (let ((compile-command (format "cd %s ; make"
-                                              (magit-toplevel))))
-                 (save-buffer)
-                 (compile compile-command))))))
+((nil (eval setq compilation-directory (magit-toplevel))))
