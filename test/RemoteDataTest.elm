00f3f83776bf4776a4ee6ab8fda79cd5ef63225c
T. Brandon Ashley <tb@tba.sh>
2017-09-09 16:28:56 -0400

    test(*): add test suite for mapBoth

--- test/RemoteDataTest.elm
+++ test/RemoteDataTest.elm
@@ -8,6 +8,7 @@ tests : Test
 tests =
     suite "RemoteData"
         [ mapTests
+        , mapBothTests
         , prismTests
         ]
 
@@ -26,6 +27,23 @@ mapTests =
                 , ( NotAsked, NotAsked )
                 , ( Loading, Loading )
                 , ( Failure "error", Failure "error" )
+                ]
+
+
+mapBothTests : Test
+mapBothTests =
+    let
+        check ( input, output ) =
+            assertEqual output
+                (mapBoth ((*) 3) ((++) "error") input)
+    in
+        suite "mapBoth"
+            <| List.map defaultTest
+            <| List.map check
+                [ ( Success 2, Success 6 )
+                , ( NotAsked, NotAsked )
+                , ( Loading, Loading )
+                , ( Failure "", Failure "error" )
                 ]
 
 
